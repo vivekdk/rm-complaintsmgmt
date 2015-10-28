3d5a4857d660b78e3d7311e72092816e50818089
Vivek D K <vivek.dk@wearexenon.com>
2015-10-28 19:12:45 +0530

    serving the files through node.js server

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/',express.static(__dirname + '/app/'));
 app.listen(process.env.PORT || 5000);
