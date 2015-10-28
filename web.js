a7bcd49941dc69e1a75bc7c12cc45c8f0da379b5
Vivek D K <vivek.dk@wearexenon.com>
2015-10-28 19:37:55 +0530

    node.js render

--- web.js
+++ web.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.use('/',express.static(__dirname + '/app/'));
+app.use('/',express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 5000);
